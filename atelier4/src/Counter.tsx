b8fb8c01cce1982fc9586dd022d9cf0bb7e703fb
microgod <ayoubdya@gmail.com>
2023-11-13 21:15:12 +0100

    a

--- atelier4/src/Counter.tsx
+++ atelier4/src/Counter.tsx
@@ -5,12 +5,24 @@ const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="flex flex-col gap-2 justify-center items-center">
       <h1>Counter</h1>
       <div className="flex gap-2">
-        <button onClick={() => setCount((p) => p - 1)}>-</button>
-        <button className="text-red-400">{count}</button>
-        <button onClick={() => setCount((p) => p + 1)}>+</button>
+        <button
+          className="w-16 h-16 text-3xl bg-gray-100 active:translate-y-2 transition-all "
+          onClick={() => setCount((p) => p - 1)}
+        >
+          -
+        </button>
+        <div className="w-16 h-16 text-3xl font-bold bg-gray-300 rounded-lg flex items-center justify-center">
+          {count}
+        </div>
+        <button
+          className="w-16 h-16 text-3xl bg-gray-100 active:translate-y-2 transition-all "
+          onClick={() => setCount((p) => p + 1)}
+        >
+          +
+        </button>
       </div>
     </div>
   );
